09e38fb7ba2acef9c781dd28e69f0f82a3499413
amin <moammadamin1375@gmail.com>
2022-09-20 18:27:45 +0430

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+contacts = []
+
+while True:
+    ch = input("""Enter a choice:
+    0: exit
+    1=>new
+    2=>list
+    3=>search
+    """)
+
+    if ch == '0':
+        break
+    if ch == '1':
+        name = input("Enter name: ")
+        phone = input("Enter phone: ")
+        contact = [name, phone]
+        contacts.append(contact)
+    elif ch == '2':
+        print(contacts)
+    elif ch == '3':
+        name = input("Enter name: ")
+        for contact in contacts:
+            if contact[0] == name:
+                contact[1]
+     
+        
